3ddc065760dc8ac10d9a6b42d94cbaf77e728c99
ayofalo <ayoflw@gmail.com>
2017-02-03 14:09:22 +0100

    Room Allocator Commandline app

--- cliapp.js
+++ cliapp.js
@@ -1,113 +1,127 @@
 #!/usr/bin/env node
 
-//var fs = require('fs'); // The File system module is used in NodeJS  to provide Input and output functionality. 
+var fs = require('fs');
+var prompt = require('prompt');
+const readline = require('readline') //Provides an interface for reading data from a Readable stream one line at a time.
 
-const readline = require('readline') //
-
-const rl = readline.createInterface({ // I want to inteprete this. 
+const rl = readline.createInterface({ // Creates Instances of the readline.Interface.
   input: process.stdin,
   output: process.stdout,
-  prompt: '>>'
+  prompt: '>>',
+
 })
 
-function Person(name, role, wantsaccomodation, room) { // Class person with arguements name, role, accommodation and room. 
-  this.name = name; //A property of the class Personss
-  this.role = role; //A property of the class role. 
-  this.wantsaccomodation = wantsaccomodation;
-  this.room = room;
-  this.allocate = function() {
-    this.room = roomsList[Math.floor((Math.random() * roomsList.length))];
+function Person(name, role, wantsaccomodation, room) { // Class Person with arguements name, role, accommodation and room. 
+  this.name = name; // The name of the person
+  this.role = role; // The role of the person. There are only two options - Fellow or Staff.
+  this.wantsaccomodation = wantsaccomodation; // Used to indicate whether or not the person is interested in getting allocated an accomodation.
+  this.room = room; // The name of the room the person is allocated.
+  this.allocate = function() { // This method allocates a room to a person. A person can be allocated a room.
+    this.room = roomsList[Math.floor((Math.random() * roomsList.length))]; //To select a random room.
 
   };
-  this.allocateOffice = function() {
-    var n = Math.floor((Math.random() * officearray.length));
-    if (officearray[n].residentnumber < 6) { // i have to connect this part to the resident part
-      this.room = officearray[n].name;
-      officearray[n].resident.push(this.name) // addition
-      officearray[n].residentnumber = officearray[n].residentnumber + 1
+  this.allocateOffice = function() { // This method allocates an office to a Staff or Fellow. 
+    var n = Math.floor((Math.random() * officearray.length)); // Used to generate a random number.
+    if (officearray[n].residentnumber < 6) { // Used to limit the number of people that can be allocated a certain room.
+      this.room = officearray[n].name; //Used to allocate a room to a person.
+      officearray[n].resident.push(this.name) // Adding the name into the property resident.
+      officearray[n].residentnumber = officearray[n].residentnumber + 1 // Increases the number of people in a room after a person is allocated a room. 
 
 
     } else {
-      console.log("You have exceeded the room limit");
+      console.log("Maximum number of persons reached please reallocate the person"); //Used to notify the allocator that the a specific room is filled up.
     }
   }
-  this.allocateLivingSpace = function() {
-    var n = Math.floor((Math.random() * livingspacearray.length));
-    if (livingspacearray[n].residentnumber < 4) { // i have to connect this part to the resident part
-      this.room = livingspacearray[n].name;
-      livingspacearray[n].resident.push(this.name);
-      livingspacearray[n].residentnumber = livingspacearray[n].residentnumber + 1;
+  this.allocateLivingSpace = function() { // This method allocates a Livingspace to a Fellow.
+    var n = Math.floor((Math.random() * livingspacearray.length)); // Used to generate a random number.
+    if (livingspacearray[n].residentnumber < 4) { // Used to limit the number of people that can be allocated a certain room.
+      this.room = livingspacearray[n].name; //Used to allocate a room to a person.
+      livingspacearray[n].resident.push(this.name); // Adding the name of the resident into the property resident.
+      livingspacearray[n].residentnumber = livingspacearray[n].residentnumber + 1; // Increases the number of people in a room after a person is allocated a room.
     } else {
-      console.log("You have exceeded the room limit");
+      console.log("Maximum number of persons reached please reallocate the person"); //Used to notify the allocator that the a specific room is filled up.
     }
   }
 }
 
-var reallocate = function(name, room) {
+var reallocate = function(name, room) { // This is used to reallocate a staff from a room to another room
 
-  for (i = 0; i < officearray.length; i++) {
+  for (i = 0; i < officearray.length; i++) { //For loop used to find the name of the office that a person would be reallocated to
     if (room == officearray[i].name) {
-      officearray[i].resident.push(name);
-      officearray[i].residentnumber = officearray[i].residentnumber + 1
-      console.log(officearray[i].resident);
+      officearray[i].resident.push(name); //Used to add the person to a specific room 
+      officearray[i].residentnumber = officearray[i].residentnumber + 1 // Increases the number of people in a room 
+      console.log(`${name}` + " has been reallocated the Office called  " + `${room}`);
+      console.log(`${room}` + " has resident " + `${officearray[i].resident}`)
+
+
     }
   }
-  for (i = 0; i < livingspacearray.length; i++) {
+  for (i = 0; i < livingspacearray.length; i++) { //For loop used to find the name of the livingspace that a person would be reallocated to
     if (room == livingspacearray[i].name) {
-      livingspacearray[i].resident.push(name);
-      livingspacearray[i].residentnumber = livingspacearray[i].residentnumber + 1;
-      console.log(livingspacearray[i].resident);
+      livingspacearray[i].resident.push(name); //Used to add the person to a specific room 
+      livingspacearray[i].residentnumber = livingspacearray[i].residentnumber + 1; // Increases the number of people in a room
+      console.log(`${name}` + " has been reallocated the livingspace called  " + `${room}`);
+      console.log(`${room}` + " has resident " + `${livingspacearray[i].resident}`)
+
     }
   }
 };
 
 
 
-function Room(name, resident) {
+function Room(name, resident) { // Class Room was created with property name and resident.
   this.name = name;
   this.resident = resident;
 
 };
 
-function Office(name) { //a second arguement would be pushed into the resident array
-  this.name = name;
-  this.type = Office;
+function Office(name) { // Class Office was created with properrty name, type, resident and residentnumber.
+  this.name = name; // The Class Office didnt inherit from the Room because the properties are unique to this class and are flexible. If all the rooms were given a property like colour 'red'office would have inherited from it.
   this.resident = [];
   this.residentnumber = 0;
 
 };
 
-function LivingSpace(name) {
+function LivingSpace(name) { // Class Office was created with properrty name, type, resident and residentnumber.
   this.name = name;
-  this.type = Office;
   this.resident = [];
   this.residentnumber = 0;
 };
 
-var roomsList = [];
-var officeroomsList = [];
-var roomObject = [];
-var personList = [];
-var param = [];
-var sparam = [];
-var officearray = [];
-var livingspacearray = [];
-var lee = [];
-var leen = [];
+function allLetter(inputtxt) {
+  var letters = /^[A-Za-z]+$/;
+  if (inputtxt.value.match(letters)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var rooms_list = []; //  Initialization and Contains all the rooms.
+var personList = []; //Initialization and Contains an array of the person objects.
+var param = []; //Initialization and used in loading data as a variable to temporary store each string  that was splitted from a sentence in the text file.
+var sparam = []; // Takes in all the rooms
+var officearray = []; //Used to store a list of offices.
+var livingspacearray = []; //Used to store a list of livingspaces.
 
 
-rl.prompt() // resets it 
 
-rl.on('line', function(line) {
+
+rl.prompt() // resets it and resumes input stream
+
+rl.on('line', function(line) { // on entering a line an array args would be created and the line would be splited by a space to produce individual arguments.
     var args = line.split(' ')
     switch (args[0]) {
 
       // create_room <room_name>
       case 'create_room':
 
+
+
         var i = 1;
         while (i < 11) {
           //Created a Room object for each room. The name of the room is the object.
+
           var n = Math.floor(Math.random() * 2);
           if (n == 1) {
             args[i] = new Office(args[i]);
@@ -120,27 +134,31 @@ rl.on('line', function(line) {
             livingspacearray.push(args[i]);
             console.log(`Room "${args[i].name}" is a LivingSpace!`)
           }
-          // roomsList.push(args[i].name);
           i++
         };
 
         sparam.push(officearray);
         sparam.push(livingspacearray);
-        console.log(sparam);
         break
 
         // list_rooms
       case 'list_rooms':
         if (args[1] == "Office") {
+          console.log('\n');
+          console.log('List of Offices');
+          console.log('------------------');
           officearray.forEach(function(room, idx) {
             console.log(`${idx} - ${room.name}`)
           })
         } else if (args[1] == "LivingSpace") {
+          console.log('\n');
+          console.log('List of LivingSpaces');
+          console.log('------------------');
           livingspacearray.forEach(function(room, idx) {
             console.log(`${idx} - ${room.name}`)
           })
         }
-        console.log('Printing out all rooms')
+
         break
 
         //add_person
@@ -161,37 +179,31 @@ rl.on('line', function(line) {
 
               if (args[2] === "Staff") {
                 personObject.allocateOffice();
-                console.log(personObject.room);
-                console.log(officearray.length);
-                console.log(officearray);
+                console.log(`${personObject.name}` + " has been allocated the office called  " + `${personObject.room}`)
               }
 
               if (args[2] === "Fellow") {
-                rl.question('Are you interested in a LivingSpace or Office? ', (answer) => {
 
-                  console.log(`We would allocate you : ${answer}`);
+                rl.question('Are you interested in a LivingSpace or Office?: \n', function(answer) { // Experiencing challenges here. The query is printed twice instead of once.
                   if (answer === "Office") {
-                    console.log('Office man')
+                    console.log('\n');
+                    console.log('Office owner\n');
+                    console.log('------------------');
                     personObject.allocateOffice();
-                    console.log(personObject.room);
-                    console.log(officearray.length);
-                    console.log(officearray);
-                  }
-                  if (answer === "Livingspace") {
-                    console.log('Livingspace owner')
+                    console.log(`${personObject.name}` + " has been allocated the office called  " + `${personObject.room}`)
+                  } else if (answer === "Livingspace") {
+                    console.log('\n');
+                    console.log('Livingspace owner\n');
+                    console.log('------------------');
                     personObject.allocateLivingSpace();
-                    console.log(personObject.room);
-                    console.log(livingspacearray.length);
-                    console.log(livingspacearray);
+                    console.log(`${personObject.name}` + " has been allocated the Livingspace called  " + `${personObject.room}`)
                   }
-                })
-              }
-              console.log(officearray)
-              console.log(livingspacearray)
+                });
 
+              }
               break;
             case 'N':
-              console.log(" You have selected No therefore you are not going to be allocated any room. Goodluck");
+              console.log(" You have selected No, therefore you are not going to be allocated any room. Goodluck");
               break;
             default:
               console.log('You must enter a valid command');
@@ -210,23 +222,22 @@ rl.on('line', function(line) {
         break
       case 'reallocate_person':
 
-        for (i = 0; i < officearray.length; i++) {
+        for (i = 0; i < officearray.length; i++) {// The purpose of this loop is to reduce the number of residents in an office that its resident has been reallocated to another office 
           if (args[1] == officearray[i].resident) {
-            officearray[i].resident =[];
-            officearray[i].residentnumber= officearray[i].residentnumber - 1;
-            console.log(officearray[i].resident);
+            officearray[i].resident = [];
+            officearray[i].residentnumber = officearray[i].residentnumber - 1;
+          
           }
         }
-        for (i = 0; i < livingspacearray.length; i++) {
+        for (i = 0; i < livingspacearray.length; i++) {// The purpose of this loop is to reduce the number of residents in a livingspace that its resident has been reallocated to another livingspace
           if (args[2] == livingspacearray[i].resident) {
-            livingspacearray[i].resident=[];
-            livingspacearray[i].residentnumber= livingspacearray[i].residentnumber - 1;
-            console.log(livingspacearray[i].resident);
+            livingspacearray[i].resident = [];
+            livingspacearray[i].residentnumber = livingspacearray[i].residentnumber - 1;
           }
         }
 
         reallocate(args[1], args[2]);
-        console.log();
+        
         break
       case 'load_people':
         var content = fs.readFileSync('sample.txt', 'utf8');
@@ -249,9 +260,7 @@ rl.on('line', function(line) {
                 personList.push(personObject)
                 if (param[1] === "Staff") {
                   personObject.allocateOffice();
-
-                  console.log(personObject.room);
-                  console.log(officearray.length);
+                  console.log("Office "+`${personObject.room}` + " has been allocated the person above ");
 
                 }
 
@@ -260,12 +269,10 @@ rl.on('line', function(line) {
                   var r = Math.floor(Math.random() * 2);
                   if (r == 1) {
                     personObject.allocateOffice();
-                    console.log(personObject.room);
-                    console.log(officearray.length);
+                    console.log("Office "+`${personObject.room}` + " has been allocated the person above ");
                   } else {
                     personObject.allocateLivingSpace();
-                    console.log(personObject.room);
-                    console.log(livingspacearray.length);
+                    console.log("Livingspace "+`${personObject.room}` + " has been allocated the person above ");
                   }
 
                 }
@@ -320,7 +327,7 @@ rl.on('line', function(line) {
           }
         );
         break
-      case 'print_room':
+      case 'print_room'://prints the name of all the people in a particular room 
 
         for (i = 0; i < officearray.length; i++) {
           if (officearray[i].name == args[1]) {
@@ -345,14 +352,14 @@ rl.on('line', function(line) {
           for (var i = 0; i < officearray.length; i++) {
 
             var d = officearray[i].name
-            var r = officearray[i].resident
+            var r = officearray[i].resident.toString();
             stmt.run(d, r);
           }
           stmt.finalize();
           for (var i = 0; i < livingspacearray.length; i++) {
 
             var d = livingspacearray[i].name
-            var r = livingspacearray[i].resident
+            var r = livingspacearray[i].resident.toString();
             liv.run(d, r);
           }
           liv.finalize();
